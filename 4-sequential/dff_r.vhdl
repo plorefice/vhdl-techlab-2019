02462c3ce23f8302ffd940451e6c3340057cdb1d
Pietro Lorefice <pietro.lorefice@gmail.com>
2019-09-04 18:37:37 +0200

    dff_r: add initial code

---
+++ 4-sequential/dff_r.vhdl
@@ -0,0 +1,54 @@
+-- ============================================== --
+-- Positive edge-triggered D-flip-flop with reset --
+-- ============================================== --
+--                                                --
+--                        v                       --
+--                        |                       --
+--                 +-------------+                --
+--                 |     RST     |                --
+--            >----|  D       Q  |---->           --
+--                 |             |                --
+--            >----|> CLK    /Q  |---->           --
+--                 |             |                --
+--                 +-------------+                --
+--                                                --
+-- ===========                                    --
+-- Truth table                                    --
+-- ===========                                    --
+--                                                --
+--  RST  CLK  D  |  Q   /Q                        --
+--  -----------------------                       --
+--   1    X   X  |  0    1                        --
+--   0    ^   0  |  0    1                        --
+--   0    ^   1  |  1    0                        --
+--   0    X   X  |  Q   /Q                        --
+--                                                --
+----------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity dff_r is
+    port (
+        d, clk, rst : in  std_logic;
+        q, qn       : out std_logic
+    );
+end entity;
+
+architecture behavioral of dff_r is
+begin
+
+    -- The process sensitivity list in this case consists of clock and reset signals
+    process (clk, rst)
+    begin
+        -- Asynchronous reset -- takes precedence over everything else
+        if (rst = '1') then
+            q <= '0';
+            qn <= '1';
+        elsif (rising_edge(clk)) then
+            q <= d;
+            qn <= not d;
+        end if;
+    end process;
+
+end architecture;
---
+++ 4-sequential/dff_r_tb.vhdl
@@ -0,0 +1,76 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity dff_r_tb is
+end entity;
+
+architecture testbench of dff_r_tb is
+    shared variable running : boolean := true; -- whether or not the simulation is still running
+
+    constant T : time      := 40 ns; -- clock period
+    signal clk : std_logic := '0';   -- initialization value, only allowed in simulations
+    signal rst : std_logic := '1';
+    signal d, q, qn : std_logic;
+begin
+
+    uut: entity work.dff_r (behavioral)
+                port map (
+                    d   => d,
+                    clk => clk,
+                    rst => rst,
+                    q   => q,
+                    qn  => qn
+                );
+
+    -- Model clock and reset behaviour outside of the testbench process
+    clk <= not clk after T / 2 when running else '0';
+    rst <= '1', '0' AFTER T / 4;
+
+    tb: process begin
+        -- Wait for the simulation to run combinatorial logic
+        wait for 1 ps;
+
+        -- Let's start by asserting that the initial conditions are valid
+        assert q = '0'  report "Q is not 0 during reset" severity failure;
+        assert qn = '1' report "/Q is not 1 during reset" severity failure;
+
+        -- Changing stuff during reset should not affect the flip-flop behavior
+        d <= '1';
+        wait for 1 ns;
+
+        assert q = '0'  report "Q changed during reset" severity failure;
+        assert qn = '1' report "/Q changed during reset" severity failure;
+
+        -- Now we wait for the reset signal to go low
+        -- and test that changes to the value of d are still ignored
+        wait until falling_edge(rst);
+
+        assert q = '0'  report "Q changed outside of clock front" severity failure;
+        assert qn = '1' report "/Q changed outside of clock front" severity failure;
+
+        -- Next, check that the flip-flop is actually positive-edge-triggered
+        wait until rising_edge(clk);
+        wait for 0 ns; -- wait for a delta cycle
+
+        assert q = '1'  report "Q did not change on clock front" severity failure;
+        assert qn = '0' report "/Q did not change on clock front" severity failure;
+
+        -- Again, changing the D pin should have no effect
+        d <= '0';
+        wait for 1 ns;
+
+        assert q = '1'  report "Q changed outside of clock front" severity failure;
+        assert qn = '0' report "/Q changed outside of clock front" severity failure;
+
+        -- Finally, make sure nothing happens on the clock's falling edge
+        wait until falling_edge(clk);
+
+        assert q = '1'  report "Q changed on falling clock front" severity failure;
+        assert qn = '0' report "/Q changed on falling clock front" severity failure;
+
+        -- Stop the simulation by stopping the clock
+        running := false;
+        wait;
+    end process;
+
+end architecture;
