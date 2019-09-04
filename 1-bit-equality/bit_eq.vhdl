a94c0e4642103548e3c5db7df7c1fca7ebdd5c4e
Pietro Lorefice <pietro.lorefice@gmail.com>
2019-09-04 19:11:24 +0200

    all: ASCII ART :tada:

--- 1-bit-equality/bit_eq.vhdl
+++ 1-bit-equality/bit_eq.vhdl
@@ -1,12 +1,32 @@
+-- =======================================
+--          1-bit equality tester
+-- =======================================
+--
+--                 bit_eq
+--             +------------+
+--        >----| a          |
+--             |          q |---->
+--        >----| b          |
+--             +------------+
+--
+--  ===========
+--  Truth table
+--  ===========
+--
+--   a   b  |  q
+--  -------------
+--   0   0  |  1
+--   0   1  |  0
+--   1   1  |  1
+--   1   0  |  0
+--
+-- =======================================
+
 -- Bring the `ieee` library into scope to import the `ieee.std_logic_1164` and use its types.
 library ieee;
 use ieee.std_logic_1164.all;
 
--- Declare an entity called `bit_eq` having:
---  * two inputs: `a` and `b`
---  * one output: `q`
---
--- `q` should be asserted whenever `a` and `b` have the same logic state.
+-- Declare an entity called `bit_eq` having two inputs (`a` and `b`) and one output (`q`)
 entity bit_eq is
     port (
         a, b : in  std_logic;
@@ -18,16 +38,7 @@ end entity;
 architecture structural of bit_eq is
 begin
 
-    -- This is the truth table of `q`:
-    --
-    --     a  b  |  q
-    --     ----------
-    --     0  0  |  1
-    --     0  1  |  0
-    --     1  1  |  1
-    --     1  0  |  0
-    --
-    -- This translates into Q = /(A ^ B)
+    -- This codifies the above truth table
     q <=not (a xor b);
 
 end architecture;
